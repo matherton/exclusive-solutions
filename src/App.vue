92f0491c54ef67382b92a71889eafbff35b9881f
Mark Atherton <mark@insightinternet.co.uk>
2025-05-19 18:37:17 +0100

    deleted uneeded assets and added a navigation daisy component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
+<script setup>
+import navBar from "./components/navBar.vue";
+</script>
+
 <template>
+  <navBar class="fixed top-0 w-full z-50" />
   <h1 class="text-3xl font-bold underline mb-10">Hello Vue!</h1>
   <button class="btn btn-primary">Hello Daisy button</button>
 </template>
