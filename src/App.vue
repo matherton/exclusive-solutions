12da6391e043decbe8b30239d352e393dab3b5b7
Mark Atherton <mark@insightinternet.co.uk>
2025-05-20 14:19:12 +0100

    fixed nav component NOTE: check if sourcecode has classNames

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import navBar from "./components/navBar.vue";
 </script>
 
 <template>
-  <navBar class="top-0 w-full z-50 h-16" />
+  <navBar class="bg-neutral text-neutral-content p-2" />
+
   <h1 class="text-3xl font-bold underline mb-10">Hello Vue!</h1>
   <button class="btn btn-primary">Hello Daisy button</button>
 </template>
