7e154daf4b3e8d57aa9dc83983b30d07ab87894c
Mark Atherton <mark@insightinternet.co.uk>
2025-06-04 15:43:17 +0100

    got the first hero slider image displaying as a full screen width background

--- src/components/HeroSlider.vue
+++ src/components/HeroSlider.vue
@@ -1,9 +1,7 @@
 <script setup>
 import HeroSlides from "./HeroSlides.vue";
-import Camera from "./slider-images/camera.jpg";
-/* import MixingDesk from "./slider-images/mixing-desk.jpg";
-import Skateboard from "./slider-images/skateboard.jpg";
-import MixingDesk2 from "./slider-images/mixing-desk-2.jpg"; */
+import cameraImage from "@/slider-images/camera.jpg?url";
+import iphoneImage from "@/slider-images/iphone.jpg?url";
 
 const components = {
   HeroSlides,
@@ -22,36 +20,39 @@ export default {
       images: [
         {
           id: 0,
-          src: Camera,
+          src: cameraImage,
           title: "Camera",
           subText: "Capture moments",
         },
         {
           id: 1,
-          src: "/slider-images/iphone.jpg",
+          src: iphoneImage,
           title: "iPhone",
           subText: "Capture movies",
         },
         {
           id: 2,
-          src: "/slider-images/mixing-desk.jpg",
+          src: "@/slider-images/mixing-desk.jpg",
           title: "Mixing Desk",
           subText: "Mix your tracks",
         },
         {
           id: 3,
-          src: "/slider-images/mixing-desk.jpg",
+          src: "@/slider-images/mixing-desk-2.jpg",
           title: "Mixing Desk 2",
           subText: "Mix your sound",
         },
         {
           id: 4,
-          src: "/slider-images/skateboard.jpg",
+          src: "@/slider-images/skateboard.jpg",
           title: "Skateboard",
           subText: "Ride the streets",
         },
       ],
     };
+  },
+  mounted() {
+    console.log(this.images);
   },
 };
 </script>
