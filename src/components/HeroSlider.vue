923e444ec9ff4c692a95bb03e57c4000e75c7ebe
Mark Atherton <mark@insightinternet.co.uk>
2025-06-04 15:07:00 +0100

    moved slider-images into components and got the first slide to display correctly as a background

---
+++ src/components/HeroSlider.vue
@@ -0,0 +1,58 @@
+<script setup>
+import HeroSlides from "./HeroSlides.vue";
+import Camera from "./slider-images/camera.jpg";
+/* import MixingDesk from "./slider-images/mixing-desk.jpg";
+import Skateboard from "./slider-images/skateboard.jpg";
+import MixingDesk2 from "./slider-images/mixing-desk-2.jpg"; */
+
+const components = {
+  HeroSlides,
+};
+</script>
+<template lang="">
+  <div id="hero-slider">
+    <HeroSlides :image="images[0]" />
+  </div>
+</template>
+<script>
+export default {
+  name: "HeroSlider",
+  data() {
+    return {
+      images: [
+        {
+          id: 0,
+          src: Camera,
+          title: "Camera",
+          subText: "Capture moments",
+        },
+        {
+          id: 1,
+          src: "/slider-images/iphone.jpg",
+          title: "iPhone",
+          subText: "Capture movies",
+        },
+        {
+          id: 2,
+          src: "/slider-images/mixing-desk.jpg",
+          title: "Mixing Desk",
+          subText: "Mix your tracks",
+        },
+        {
+          id: 3,
+          src: "/slider-images/mixing-desk.jpg",
+          title: "Mixing Desk 2",
+          subText: "Mix your sound",
+        },
+        {
+          id: 4,
+          src: "/slider-images/skateboard.jpg",
+          title: "Skateboard",
+          subText: "Ride the streets",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/HeroSlides.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <div id="hero-slides">
+    <!-- <h2>{{ image.title }}</h2>
+    <p>{{ image.subText }}</p> -->
+    <div class="slideImage" :style="backgroundStyle"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeroSlides",
+  props: ["image"],
+  computed: {
+    backgroundStyle() {
+      if (typeof this.image.src === "string") {
+        return {
+          backgroundImage: `url(${this.image.src})`,
+        };
+      } else {
+        return {
+          backgroundImage: `url(${require("@/slider-images/" +
+            this.image.src)})`,
+        };
+      }
+    },
+  },
+};
+</script>
+<style>
+#hero-slides .slideImage {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+}
+</style>
