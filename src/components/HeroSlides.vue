f23079580eb3efea3e2bccb9bc59967ac3508d11
Mark Atherton <mark@insightinternet.co.uk>
2025-06-05 11:54:16 +0100

    got the slide to cycle through images and used flexbox to position text in centre

--- src/components/HeroSlides.vue
+++ src/components/HeroSlides.vue
@@ -1,8 +1,14 @@
 <template lang="">
   <div id="hero-slides">
-    <!-- <h2>{{ image.title }}</h2>
-    <p>{{ image.subText }}</p> -->
-    <div class="slideImage" :style="backgroundStyle"></div>
+    <transition-group name="fade">
+      <div class="slideImage" :style="backgroundStyle">
+        <div class="slideTitle">
+          <h2>{{ image.title }}</h2>
+
+          <p>{{ image.subText }}</p>
+        </div>
+      </div>
+    </transition-group>
   </div>
 </template>
 <script>
@@ -27,10 +33,27 @@ export default {
 </script>
 <style>
 #hero-slides .slideImage {
+  height: 100vh;
+  width: 100%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+}
+#hero-slides .slideImage .slideTitle {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: 2rem;
+  font-weight: bold;
   height: 100vh;
-  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+#hero-slides .slideTitle h2 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+#hero-slides .slideTitle p {
+  font-size: 1.5rem;
 }
 </style>
