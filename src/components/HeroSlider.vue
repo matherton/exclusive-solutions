bb3abc308f0e9e0d05b749642f6b4486ec6ac00b
Mark Atherton <mark@insightinternet.co.uk>
2025-06-24 12:44:55 +0100

    Changed the HeroSlider to slide between transitions Note going to copy the HeroSlider on this branch so that I can create this as a new component on the main branch

--- src/components/HeroSlider.vue
+++ src/components/HeroSlider.vue
@@ -1,155 +1,128 @@
-<script setup>
-import HeroSlides from "./HeroSlides.vue";
-import cameraImage from "@/slider-images/camera.jpg?url";
-import iphoneImage from "@/slider-images/iphone.jpg?url";
-import mixingDeskImage from "@/slider-images/mixing-desk.jpg?url";
-import mixingDesk2Image from "@/slider-images/mixing-desk2.jpg?url";
-import skateboardImage from "@/slider-images/skateboard.jpg?url";
-
-const components = {
-  HeroSlides,
-};
-</script>
-<template lang="">
-  <div class="hero-slider-container">
-    <div class="hero-slider">
-      <div class="arrowRight" @click="moveRight">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-24 w-24"
-          fill="white"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="white"
+<template>
+  <div class="relative w-full h-full overflow-hidden">
+    <div
+      class="flex transition-transform duration-500 ease-in-out h-full"
+      :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+    >
+      <div
+        v-for="(slide, i) in slides"
+        :key="i"
+        class="w-full h-full flex-shrink-0 bg-cover bg-center relative"
+        :style="{ backgroundImage: `url(${slide.img})` }"
+      >
+        <!-- Overlay text -->
+        <div
+          class="absolute inset-0 flex flex-col items-center justify-center bg-black/40 text-white"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M10 4l6 6-6 6z"
-          />
-        </svg>
-      </div>
-
-      <HeroSlides :image="images[chooseImage]" />
-      <div class="arrowLeft" @click="moveLeft">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-24 w-24"
-          fill="white"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="white"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M14 4l-6 6 6 6z"
-          />
-        </svg>
+          <h2 class="text-3xl font-bold mb-2">{{ slide.title }}</h2>
+          <p class="text-lg">{{ slide.text }}</p>
+        </div>
       </div>
     </div>
+    <!-- Controls -->
+    <button
+      @click="prevAndReset"
+      class="btn btn-white absolute left-4 top-1/2 -translate-y-1/2 z-10"
+      aria-label="Previous slide"
+      title="Previous slide"
+      alt="previous slide control"
+    >
+      <
+    </button>
+    <button
+      @click="nextAndReset"
+      class="btn btn-white absolute right-4 top-1/2 -translate-y-1/2 z-10"
+      aria-label="Next slide"
+      title="Next slide"
+      alt="next slide control"
+    >
+      >
+    </button>
   </div>
 </template>
-<script>
-export default {
-  name: "HeroSlider",
-  data() {
-    return {
-      images: [
-        {
-          id: 0,
-          src: cameraImage,
-          title: "Camera",
-          subText: "Capture moments",
-        },
-        {
-          id: 1,
-          src: iphoneImage,
-          title: "iPhone",
-          subText: "Capture movies",
-        },
-        {
-          id: 2,
-          src: mixingDeskImage,
-          title: "Mixing Desk",
-          subText: "Mix your tracks",
-        },
-        {
-          id: 3,
-          src: mixingDesk2Image,
-          title: "Mixing Desk 2",
-          subText: "Mix your sound",
-        },
-        {
-          id: 4,
-          src: skateboardImage,
-          title: "Skateboard",
-          subText: "Ride the streets",
-        },
-      ],
-      chooseImage: 0,
-    };
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import camera from "../slider-images/camera.jpg";
+import iphone from "../slider-images/iphone.jpg";
+import mixinDesk from "../slider-images/mixing-desk.jpg";
+import mixinDesk2 from "../slider-images/mixing-desk2.jpg";
+import skateboard from "../slider-images/skateboard.jpg";
+
+const images = [camera, iphone, mixinDesk, mixinDesk2, skateboard];
+const slides = [
+  {
+    img: camera,
+    title: "Welcome",
+    text: "This is the first slide",
   },
-  methods: {
-    move() {
-      if (this.chooseImage >= this.images.length - 1) {
-        this.chooseImage = 0; // Reset to first image
-      } else {
-        this.chooseImage++; // Move to next image
-      }
-    },
-    moveRight() {
-      if (this.chooseImage >= this.images.length - 1) {
-        this.chooseImage = 0; // Reset to first image
-      } else {
-        this.chooseImage++; // Move to next image
-      }
-    },
-    moveLeft() {
-      if (this.chooseImage === 0) {
-        this.chooseImage = this.images.length - 1;
-      } else {
-        this.chooseImage--;
-      }
-      // Uncomment the line below to log the current image index
-      // console.log("Current image index:", this.chooseImage);
-      // Uncomment the line below to log the current image title
-      // console.log("Current image title:", this.images[this.chooseImage].title);
-      // Uncomment the line below to log the current image subText
-      // console.log("Current image subText:", this.images[this.chooseImage].subText);
-      // Uncomment the line below to log the current image src
-      // console.log("Current image src:", this.images[this.chooseImage].src);
-      // Uncomment the line below to log the current image id
-      // console.log("Current image id:", this.images[this.chooseImage].id);
-    },
+  {
+    img: iphone,
+    title: "Discover",
+    text: "This is the second slide",
   },
-  created() {
-    setInterval(() => {
-      this.move();
-    }, 4000); // Change image every 4 seconds
+  {
+    img: mixinDesk,
+    title: "Enjoy",
+    text: "This is the third slide",
   },
-};
+  {
+    img: mixinDesk2,
+    title: "Explore",
+    text: "This is the fourth slide",
+  },
+  {
+    img: skateboard,
+    title: "Experience",
+    text: "This is the fifth slide",
+  },
+];
+const currentIndex = ref(0);
+let intervalId = null;
+
+function next() {
+  currentIndex.value = (currentIndex.value + 1) % images.length;
+}
+function prev() {
+  currentIndex.value = (currentIndex.value - 1 + images.length) % images.length;
+}
+
+// Auto-slide logic
+function startAutoSlide() {
+  stopAutoSlide();
+  intervalId = setInterval(() => {
+    next();
+  }, 4000);
+}
+function stopAutoSlide() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}
+function nextAndReset() {
+  next();
+  startAutoSlide();
+}
+function prevAndReset() {
+  prev();
+  startAutoSlide();
+}
+
+onMounted(() => {
+  startAutoSlide();
+});
+onBeforeUnmount(() => {
+  stopAutoSlide();
+});
 </script>
-<style>
-.hero-slider {
-  position: relative;
-}
 
-.arrowRight {
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translateY(-50%);
-  z-index: 10; /* Ensure the arrow is above the slides */
-  cursor: pointer;
+<style scoped>
+/* Ensure each slide fills the container */
+.w-full {
+  width: 100%;
 }
-
-.arrowLeft {
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-  z-index: 10; /* Ensure the arrow is above the slides */
-  cursor: pointer;
+.h-full {
+  height: 100%;
 }
 </style>
